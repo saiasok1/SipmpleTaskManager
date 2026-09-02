8b26dd49cb1676f4b2341a3cd3d5d451e67f7813
saiasok1 <saiasok1k@gmail.com>
2026-09-02 16:56:48 +0530

    Added TaskItem Model

---
+++ Models/TaskItem.cs
@@ -0,0 +1,9 @@
+namespace SimpleTaskManager.Models;
+
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+
+        public bool IsCompleted { get; set; }
+    }
--- Program.cs
+++ Program.cs
@@ -1,41 +1,37 @@
+using SimpleTaskManager.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.MapOpenApi();
+// }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/tasks", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var task = new TaskItem
+    {
+        Id = 1, 
+        Title = "Learn .NET", 
+        IsCompleted = false 
+        
+    };
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    return task;
+});
+
+
+
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
