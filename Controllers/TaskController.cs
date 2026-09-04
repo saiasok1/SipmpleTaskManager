61809396d1ef0ab51e4cf74a124ca8361950d91f
saiasok1 <saiasok1k@gmail.com>
2026-09-04 12:46:09 +0530

    Move get task to controller

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -8,9 +8,35 @@ namespace SimpleTaskManager.Controllers;
 
 public class TaskController : ControllerBase
 {
+    private readonly List<TaskItem> tasks = new()
+    {
+        new TaskItem
+        {
+            Id = 1,
+            Title = "Learn C#",
+            IsCompleted = true
+        },
+
+        new TaskItem
+        {
+        Id = 2,
+        Title = "Learn .NET",
+        IsCompleted = false
+        },
+
+        new TaskItem
+        {
+            Id = 3,
+            Title = "Build my first API",
+            IsCompleted = false
+        }
+    };
+
+    //GET Action 
+    //GET /tasks
     [HttpGet]
     public IActionResult GetTasks()
     {
-        return Ok();
+        return Ok(tasks);
     }
 }
--- Program.cs
+++ Program.cs
@@ -16,90 +16,62 @@ app.MapControllers();
 // }
 
 // app.UseHttpsRedirection();
-var tasks = new List<TaskItem>
-{
-    new TaskItem
-    {
-        Id = 1,
-        Title = "Learn C#",
-        IsCompleted = true
-    },
 
-    new TaskItem
-    {
-    Id = 2,
-    Title = "Learn .NET",
-    IsCompleted = false
-    },
 
-    new TaskItem
-    {
-        Id = 3,
-        Title = "Build my first API",
-        IsCompleted = false
-    }
-};
+// app.MapGet("/tasks/{id}", (int id) =>
+// {
+//     var task = tasks.Find(task => task.Id == id);
 
+//     if (task == null)
+//     {
+//         return Results.NotFound();
+//     }
+
+//     return Results.Ok(task);
+// });
+
+// app.MapGet("/square/{number}", (int number) =>
+// {
+//     return Results.Ok(number * number);
     
-app.MapGet("/tasks", () =>
-{
-    return tasks;
-});
-
-app.MapGet("/tasks/{id}", (int id) =>
-{
-    var task = tasks.Find(task => task.Id == id);
-
-    if (task == null)
-    {
-        return Results.NotFound();
-    }
-
-    return Results.Ok(task);
-});
-
-app.MapGet("/square/{number}", (int number) =>
-{
-    return Results.Ok(number * number);
-    
-});
+// });
 
 
-app.MapPost("/tasks", (TaskItem task)=>
-{
-    task.Id = tasks.Count + 1;
-    tasks.Add(task);
+// app.MapPost("/tasks", (TaskItem task)=>
+// {
+//     task.Id = tasks.Count + 1;
+//     tasks.Add(task);
 
-    return Results.Created($"/tasks/{task.Id}", task);
-});
+//     return Results.Created($"/tasks/{task.Id}", task);
+// });
 
-app.MapPut("/tasks/{id}", (int id, TaskItem updatedTask) =>
-{
-    var task = tasks.Find(task => task.Id == id);
+// app.MapPut("/tasks/{id}", (int id, TaskItem updatedTask) =>
+// {
+//     var task = tasks.Find(task => task.Id == id);
 
-    if (task == null)
-    {
-        return Results.NotFound();
-    }
+//     if (task == null)
+//     {
+//         return Results.NotFound();
+//     }
 
-    task.Title = updatedTask.Title;
-    task.IsCompleted = updatedTask.IsCompleted;
+//     task.Title = updatedTask.Title;
+//     task.IsCompleted = updatedTask.IsCompleted;
 
-    return Results.Ok(task);
-});
+//     return Results.Ok(task);
+// });
 
 
-app.MapDelete("/tasks/{id}", (int id) =>
-{
-    var task = tasks.Find(task => task.Id == id);
-    if (task == null)
-    {
-        return Results.NotFound();
-    }
+// app.MapDelete("/tasks/{id}", (int id) =>
+// {
+//     var task = tasks.Find(task => task.Id == id);
+//     if (task == null)
+//     {
+//         return Results.NotFound();
+//     }
 
-    tasks.Remove(task);
-    return Results.NoContent();
-});
+//     tasks.Remove(task);
+//     return Results.NoContent();
+// });
 
 
 
